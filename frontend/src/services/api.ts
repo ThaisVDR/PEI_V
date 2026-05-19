f25d3015516f37fe18fac1f77e01f587e99c5ba4
João Vitor <joaoocosta161@gmail.com>
2026-05-19 13:14:12 -0400

    api voltada para o deploy do backend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
-export const API_URL = "http://192.168.18.68:8080/api";
+//export const API_URL = "http://192.168.18.68:8080/api";
+
+export const API_URL = "http://https://questio-backend.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
